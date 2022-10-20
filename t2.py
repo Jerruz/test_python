5408d8a1a27daac7bb2f4407ed36753afc378a8e
Jerruz <jw.buzd@gmail.com>
2022-10-20 17:35:05 +0300

    commit 201022_2

--- t2.py
+++ t2.py
@@ -39,4 +39,7 @@ print(b)
 print('--////////===========================================================================')
 
 
-d = ['']
+s = "28212" 
+print(s.isdigit())
+# contains alphabets and spaces s = "Mo3 nicaG el l22er"
+print(s.isdigit())
