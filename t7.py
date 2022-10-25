1ac0b8a583384ffedc3c8bb1945639409b26af3b
Jerruz <92015202+Jerruz@users.noreply.github.com>
2022-10-25 18:22:25 +0300

    Delete t7.py

--- t7.py
+++
@@ -1,7 +0,0 @@
-ls = ['яблоко','персик','апельсин','мандарин','банан']
-
-for ind, itm in enumerate(ls, start=1):
-    if ind % 2 == 0 :
-        ls[ind - 1] = 'четное'
-        # print(ind, itm)
-print(f"{'-'.join(ls)}")
