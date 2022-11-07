1afcadcffff17ecc42b094aec72adfe4424768cc
Jerruz <jw.buzd@gmail.com>
2022-11-07 16:13:36 +0300

    commit 071122_16:14

--- t5.py
+++ t5.py
@@ -13,14 +13,17 @@
 # tot = ' '.join(new_list)
 # print(tot)
 
-str = ['привет сказочный мир', 'здравствуй моя планета']
-new_str = []
+from turtle import pensize
 
-for i in str:
-    print(['-'.join(i.split())])
-    new_str.extend(['-'.join(i.split())])
 
-print(new_str)
+# str = ['привет сказочный мир', 'здравствуй моя планета']
+# new_str = []
+
+# for i in str:
+#     print(['-'.join(i.split())])
+#     new_str.extend(['-'.join(i.split())])
+
+# print(new_str)
 
 # print('*****'.join(str))
 
@@ -33,3 +36,55 @@ print(new_str)
 
 
 # print('-'.join(str2.split()))
+
+print('=======================================================')
+
+
+dic1 = {
+    'name': 'pol',
+    'sername': 'lansky',
+    'age': '55',
+    'position': 'manager'
+}
+
+a1 = sorted(dic1.items())  # сортировка словаря по ключу
+
+
+print(dict(a1))
+
+dic2 = {
+    'name': 'pol',
+    'sername': 'lansky',
+    'age': '55',
+    'position': 'manager'
+}
+print('61============================================')
+ddd = {}
+# d = list(dic2)
+for i in list(dic2):
+    d33 = {i:dic2[i]}
+    ddd.update(d33)
+print(ddd)
+
+print({ddd.update({i:dic2[i]}) for i in list(dic2)})
+
+print('63============================================')
+
+
+a2 = sorted(dic2.items(), key=lambda x: x[1])  # сортировка словаря по значению. Можно написать пользовательскую функцию вместо lambda
+
+
+print(dict(a2))
+
+print('67=======================================================')
+
+
+welcome = lambda user: print('Welcome, ' + user + '!')
+welcome('Anon')
+
+print('85=======================================================')
+
+
+
+
+
