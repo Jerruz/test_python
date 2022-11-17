961da1d959a86e7ae478d66f6b1af04243472a7f
Jerruz <jw.buzd@gmail.com>
2022-11-17 17:27:29 +0300

    171122_17:27

---
+++ t9.py
@@ -0,0 +1,15 @@
+﻿dic = {'a':[1,2,3], 'b':[11,22,33]}
+
+# for i in ['a','b']:
+#     dic[i].append(444)
+
+
+# for i in ['c']:
+#     dic[i].extend([555, 777, 888])
+
+dic.setdefault('c', {})
+
+dic['c'].update({'f':['pit','pol']})
+
+print(dic)
+
