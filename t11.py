721f8e5546e52367c6ea33845e88b9b51e6c07c6
Jerruz <jw.buzd@gmail.com>
2022-12-20 09:27:08 +0300

    201222_9:27

--- t11.py
+++ t11.py
@@ -1,8 +1,15 @@
-﻿import sys
-from pprint import pprint
+﻿# import sys
+# from pprint import pprint
+
+# # pprint(sys.path)
+
+# sys.path.append(r'C:\Users\mutovkin_av\Desktop\Груша брендбук')  # не отформатированный текст, чтобы не экранировать символ \
 
 # pprint(sys.path)
 
-sys.path.append(r'C:\Users\mutovkin_av\Desktop\Груша брендбук')  # не отформатированный текст, чтобы не экранировать символ \
 
-pprint(sys.path)
+ls_f = ['петя', 'маша','вася']
+
+ns = 'Привет: {1:<15}, {0} '.format(*ls_f)
+
+print(ns)
