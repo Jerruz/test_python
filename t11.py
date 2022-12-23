191fbe847244e84cfbefcff96f6a1b03ccf839cd
Jerruz <jw.buzd@gmail.com>
2022-12-23 17:24:39 +0300

    231222_17:25

--- t11.py
+++ t11.py
@@ -14,11 +14,39 @@
 
 # print('{0[name]} {0[sex]} {0[age]}'.format(dic))  # подстановка из словаря
 
-lst = [1,3,5,7,8,9]
+# lst = [1,3,5,7,8,9]
 
-lst[1:3] = lst[-3:-1]  # можно заменить один срез другим
-print(lst)
+# lst[1:3] = lst[-3:-1]  # можно заменить один срез другим
+# print(lst)
 
 # print(lst[-3:-1])
+
+
+# d = dict([('qwe', 5), ('asd', 7)])
+
+# print(d)
+
+# l1 = ['k1', 'k2', 'k3']
+# l2 = ['v1', 'v2', 'v3']
+
+# res = zip(l1, l2)
+
+# print(dict(list(res)))
+
+keys = ['1qwe','2asd','3zxc']
+
+a = dict.fromkeys(keys, 'qwedrt')
+
+
+# a.update({'asd' : 'uu4uuu'})
+# a.update({'asd' : 'uu45uuu'})
+
+print(a)
+
+
+
+
+
+
 
 
